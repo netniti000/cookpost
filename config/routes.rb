a994b8fec4f9059fe7132dbd13bd8bac2d1630f9
netniti000 <netniti000@yahoo.co.jp>
2020-06-12 17:36:08 +0900

    ログインページのviewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get :signup, to: 'users#new'
   resources :users
+
+  get :login, to: 'sessions#new'
+  post :login, to: 'sessions#create'
+  delete :logout, to: 'sessions#destroy'
 end
   
