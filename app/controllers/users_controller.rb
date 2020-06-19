47b8fb8b38b35d23ebb0c2df56ba16c0f9767c37
netniti000 <netniti000@yahoo.co.jp>
2020-06-19 11:50:04 +0900

    login機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,3 +25,4 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  get :about, to: 'static_pages#about'
+  
   get 'sessions/new'
   get :signup, to: 'users#new'
   resources :users
